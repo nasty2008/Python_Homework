bc6eed85b95c7dbd991417b32958e590a4f74e78
nasty-2008@mail.ru <Anastasiay>
2022-11-17 17:07:43 +0700

    5

--- tutorial_env/Scripts/activate.bat
+++ tutorial_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Desktop\1111\Python_Homework\tutorial_env
+set VIRTUAL_ENV=C:\Users\User\Desktop\ll\Python_Homework\tutorial_env
 
 if not defined PROMPT set PROMPT=$P$G
 
